f0119b7404cf0a2e7c9fbd7567dfafff28f92d59
소태호 <sth9057@gmail.com>
2024-12-03 10:37:56 +0900

    background 크기 조정

--- config.js
+++ config.js
@@ -1,9 +1,11 @@
 const config = {
-    GOOGLE_TRANSLATE_API_KEY: GOOGLE_TRANSLATE_API_KEY,
-    OPEN_WEATHER_API_KEY: OPEN_WEATHER_API_KEY,
-    OPENAI_API_KEY: OPEN_API_KEY
+    GOOGLE_TRANSLATE_API_KEY: "AIzaSyDg8-C9zhuUcATr03VujkBlJmKmizmKoMY",
+    OPEN_WEATHER_API_KEY: "cc9e4d727447096647e5a98afc0a32b0",
+    OPENAI_API_KEY:"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"
 
 };
 
 // 다른 파일에서 사용할 수 있도록 내보내기
 export default config;
+
+
